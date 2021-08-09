c251cddf7807fff9fab1b695ecc2936588491b51
Biswajit Sarker <86579868+Bi736@users.noreply.github.com>
2021-08-09 21:31:06 +0600

    Add files via upload

---
+++ mobile.js
@@ -0,0 +1,20 @@
+var circle =document.getElementById('round');
+var upBtn =document.getElementById('upbtn');
+var downBtn =document.getElementById('downbtn');
+
+var rotateValue = circle.style.transform;
+var rotateSum
+
+upBtn.onclick=function()
+{
+    rotateSum=rotateValue + 'rotate(-90deg)';
+    circle.style.transform=rotateSum;
+    rotateValue=rotateSum;
+}
+
+downBtn.onclick=function()
+{
+    rotateSum=rotateValue + 'rotate(90deg)';
+    circle.style.transform=rotateSum;
+    rotateValue=rotateSum;
+}
